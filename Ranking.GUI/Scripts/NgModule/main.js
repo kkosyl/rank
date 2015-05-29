af3cc871393e765dc03f882ace719e8289b3964e
Kamil Kosyl <kamilkosyl@gmail.com>
2015-05-29 22:14:15 +0200

    FindingPlaces

---
+++ Ranking.GUI/Scripts/NgModule/main.js
@@ -0,0 +1,23 @@
+﻿(function () {
+    var myModule = angular.module('myModule', ['ui.bootstrap']);
+
+    myModule.controller('searchCtrl', ['$scope', '$http', '$window', function ($scope, $http, $window) {
+        $scope.selected = "";
+
+        $http.get('/Place/GetPlaces')
+        .success(function (data) {
+            $scope.places = data;
+        });
+
+        $scope.onSelect = function ($item, $model, $label) {
+            $scope.$item = $item;
+            $scope.$model = $model;
+            $scope.$label = $label;
+            $window.location.href = '/Place/Details/' + $item.PlaceId;
+        };
+    }]);
+
+    myModule.controller('collapseCtrl', function ($scope) {
+        $scope.isCollapsed = true;
+    });
+})();
